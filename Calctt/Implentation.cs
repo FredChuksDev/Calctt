65bab53da44ebfd0ff15e910778e4eaffab8df69
Fred Chuks <Fred Chuks@FRED-CHUKS>
2022-08-19 18:47:17 +0100

    Add project files.

---
+++ Calctt/Implentation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calctt
+{
+    internal class Implentation
+    {
+
+
+        //-----Class Attributes
+        public string Course;
+        public int Unit;
+        public int point;
+        public string grade;
+
+        //----Method that helps calculate total score
+        public int CalcFunc(string sCourse, int ssUnit, int sPoint)
+        {
+
+            Course = sCourse;
+            Unit = ssUnit;
+            point = sPoint;
+            int Total = Unit * point;
+            return Total;
+
+
+        }
+
+        //----Method that helps calculate grade
+        public string GradeCalc(int sPoint)
+        {
+            int pp = sPoint;
+
+            if (pp < 0 || pp > 5)
+            {
+                this.grade = "Wrong Input";
+            }
+            else if (pp == 1)
+            {
+                this.grade = "E";
+            }
+            else if (pp == 2)
+            {
+                this.grade = "D";
+            }else if(pp == 3)
+            {
+                this.grade = "C";
+            }else if(pp == 4)
+            {
+                this.grade ="B";
+            }
+            else
+            {
+                this.grade = "A";
+            }
+            return this.grade;
+        }
+    }
+}
---
+++ Calctt/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calctt
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //------Objects
+            var Course1 = new Implentation();
+            var Course2 = new Implentation();
+            var Course3 = new Implentation();
+            var Course4 = new Implentation();
+            var Course5 = new Implentation();
+            var Course6 = new Implentation();
+            var Course7 = new Implentation();
+           
+
+
+
+
+            //-----Results
+            Console.WriteLine("Total");
+            Console.WriteLine(Course1.CalcFunc("ACC 102", 2, 4));
+            Console.WriteLine(Course2.CalcFunc("PSY 101", 2, 4));
+            Console.WriteLine(Course3.CalcFunc("BF 101", 2, 1));
+            Console.WriteLine(Course2.CalcFunc("MTH 112", 2, 3));
+            Console.WriteLine(Course4.CalcFunc("MAN 142", 3, 5));
+            Console.WriteLine(Course5.CalcFunc("GSP 102", 2, 2));
+            Console.WriteLine(Course2.CalcFunc("GSP 106", 2, 5));
+            
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
